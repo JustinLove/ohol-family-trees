b5920a626c92dbfd9db5bc2c14aacc86e5d73547
Justin Love <git@JustinLove.name>
2018-11-19 10:07:02 -0600

    iterate all servers for past week

--- my_past_week.rb
+++ my_past_week.rb
@@ -3,32 +3,32 @@ require 'history'
 require 'graph'
 require 'date'
 
-lives = History.new
+(1..15).each do |server|
+  lives = History.new
 
-server = "14"
-dir = "cache/lifeLog_server#{server}.onehouronelife.com"
-lives.load_dir(dir)
+  dir = "cache/lifeLog_server#{server}.onehouronelife.com"
+  lives.load_dir(dir)
 
-p lives.length
+  p lives.length
 
-focus = History.new
+  focus = History.new
 
-wondible = 'e45aa4e489b35b6b0fd9f59f0049c688237a9a86'
+  wondible = 'e45aa4e489b35b6b0fd9f59f0049c688237a9a86'
+  from = (Date.today - 1).to_time.to_i
+  to = (Date.today - 0).to_time.to_i
 
-from = (Date.today - 1).to_time.to_i
-to = (Date.today - 0).to_time.to_i
-
-lives.select do |life|
-  if life.hash == wondible
-    life.highlight = true
-    p [life.id, life.name, Time.at(life.time)]
-    if life.time > from && life.time < to
-      family = lives.family(life)
-      focus.merge!(family)
+  lives.select do |life|
+    if life.hash == wondible
+      life.highlight = true
+      p [life.id, life.name, Time.at(life.time)]
+      if life.time > from && life.time < to
+        family = lives.family(life)
+        focus.merge!(family)
+      end
     end
   end
+
+  p focus.length
+
+  Graph.graph(focus).output("output/wondible#{server}.gv", 'dot')
 end
-
-p focus.length
-
-Graph.graph(focus).output("wondible#{server}.gv", 'dot')
