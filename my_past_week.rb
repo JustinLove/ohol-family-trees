e4134542e706afc88f43480a62bebc02cb7211e9
Justin Love <git@JustinLove.name>
2018-11-19 10:23:16 -0600

    output each line in spearate file

--- my_past_week.rb
+++ my_past_week.rb
@@ -11,7 +11,7 @@ require 'date'
 
   p lives.length
 
-  focus = History.new
+  lines = {}
 
   wondible = 'e45aa4e489b35b6b0fd9f59f0049c688237a9a86'
   from = (Date.today - 1).to_time.to_i
@@ -22,13 +22,18 @@ require 'date'
       life.highlight = true
       p [life.id, life.name, Time.at(life.time)]
       if life.time > from && life.time < to
-        family = lives.family(life)
-        focus.merge!(family)
+        eve = lives.ancestors(life).last
+        unless lines[eve.id]
+          lines[eve.id] = lives.family(eve)
+        end
+        #family = lives.family(life)
+        #focus.merge!(family)
       end
     end
   end
 
-  p focus.length
-
-  Graph.graph(focus).output("output/wondible#{server}.gv", 'dot')
+  lines.each do |id,line|
+    eve = line[id]
+    Graph.graph(line).output("output/#{eve.name}.gv", 'dot')
+  end
 end
