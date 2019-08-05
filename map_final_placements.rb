d6528f0c9289ee4199a3ad419b51a53b9a742d32
Justin Love <git@JustinLove.name>
2019-08-05 11:17:35 -0500

    try data per grid location, file is larger

--- map_final_placements.rb
+++ map_final_placements.rb
@@ -5,7 +5,7 @@ require 'fileutils'
 
 include OHOLFamilyTrees
 
-output_dir = 'output/mapfinalimage'
+output_dir = 'output/mapfinalplacements'
 
 FileUtils.mkdir_p(output_dir)
 
@@ -18,39 +18,28 @@ MaplogCache::Servers.new.each do |logs|
 
   logs.each do |logfile|
     #next unless logfile.path.match('1151446675seed')
-    map = Hash.new {|h,k| h[k] = []}
+    map = Hash.new {|h,k| h[k] = {}}
     p logfile
     file = logfile.open
     while line = file.gets
       log = Maplog.create(line)
+
       if log.kind_of?(Maplog::Placement)
         tilex = log.x / tile_width
         tiley = (-log.y / tile_width)
-        ox = log.x - (tilex * tile_width)
-        oy = -(log.y - (-tiley * tile_width))
-        if map[[tilex,tiley]][ox].nil?
-          map[[tilex,tiley]][ox] = []
-        end
-        map[[tilex,tiley]][ox][oy] = log.object
+        map[[tilex,tiley]]["#{log.x} #{log.y}"] = log.object
       end
     end
     basename = logfile.path.split(/[\/]/)[1].sub('.txt', '')
     map.each do |coords,tile|
       tilex, tiley = *coords
       FileUtils.mkdir_p("#{output_dir}/#{basename}/#{tilex}")
-      list = []
-      (0..255).each do |x|
-        (0..255).each do |y|
-          if tile[x] && tile[x][y]
-            list << tile[x][y]
-          else
-            list << '.'
-          end
-        end
-      end
       File.open("#{output_dir}/#{basename}/#{tilex}/#{tiley}.txt", 'wb') do |out|
-        out << list.join(' ')
+        tile.each do |key,value|
+          out << "#{key} #{value}\n"
+        end
       end
     end
   end
+
 end
